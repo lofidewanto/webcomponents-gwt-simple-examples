da0b894b570466411cc72c1238e30c4fa1f4dfb9
lofidewanto <lofidewanto@gmail.com>
2021-03-03 23:16:05 +0100

    update MyButton to become simple just like WC

--- my-button-j2cl-es6-example/src/main/java/com/github/lofi/client/components/MyButton.java
+++ my-button-j2cl-es6-example/src/main/java/com/github/lofi/client/components/MyButton.java
@@ -1,68 +1,22 @@
 package com.github.lofi.client.components;
 
-import java.util.ArrayList;
+import java.util.logging.Logger;
 
 import elemental2.dom.HTMLElement;
 import elemental2.dom.ShadowRoot;
 import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
 
-// this means we can have at most one constructor defined, at least as far as JS is concerned,
-// and that the ctor will be the main way to create intstances
 @JsType
 public class MyButton extends HTMLElement {
 
+    private static Logger logger = Logger.getLogger(MyButton.class.getName());
+
     private static final String LABEL = "label";
 
-    private String field1;
-
-    // this _may_ be lost when used as an es5 type, don't risk it
-    private String field2 = "hasValueFromInitializer";
-
-    private int field3;
-
-    // this _may_ be lost when used as an es5 type, don't risk it
-    private int field4 = 1;
-
-    private ArrayList<String> field5;
-    
-    // this will be lost when used as an es5 type
-    private ArrayList<String> field6 = new ArrayList<String>();
-
     public MyButton() {
-        // HTMLElement.AttachShadowOptionsType options = new AttachShadowOptionsType() {
-        //
-        // private String mode;
-        //
-        // @Override
-        // public void setMode(String mode) {
-        // this.mode = mode;
-        // }
-        //
-        // @Override
-        // public String getMode() {
-        // return this.mode;
-        // }
-        // };
-        //
-        // options.setMode("open");
-        // ShadowRoot shadowRoot = attachShadow(options);
-        // shadowRoot.innerHTML = "<div class=\"container\"><button>My
-        // Button</button></div>";
+        logger.info("MyButton constructor...");
 
-        // these will all be lost when used as an es5 type
-        field1 = "hasValueFromCtor";
-        field3 = field1.length();
-        field5 = new ArrayList<String>();
-    }
-
-    public void init() {
-        field1 = "hasValueFromCtor";
-        field3 = field1.length();
-        field5 = new ArrayList<String>();
-    }
-
-    public void connectedCallback() {
         HTMLElement.AttachShadowOptionsType options = new AttachShadowOptionsType() {
 
             private String mode;
@@ -80,7 +34,7 @@ public class MyButton extends HTMLElement {
 
         options.setMode("open");
         ShadowRoot shadowRoot = attachShadow(options);
-        shadowRoot.innerHTML = "<div class=\"container\"><button>My Button</button></div>";
+        shadowRoot.innerHTML = "<div class=\"container\"><button>My Button ES6</button></div>";
         shadowRoot.firstElementChild.setAttribute("title", toString());
     }
 
@@ -103,10 +57,5 @@ public class MyButton extends HTMLElement {
     @Override
     public Object attributeChangedCallback(String attributeName, String oldValue, String newValue, String namespace) {
         return super.attributeChangedCallback(attributeName, oldValue, newValue, namespace);
-    }
-
-    @Override
-    public String toString() {
-        return field1 + field2 + field3 + field4 + field5 + field6;
     }
 }
